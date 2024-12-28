1035ee332b246c51253e16c6c0588d4e1e66d920
jorcal123 <jcalambas5@gmail.com>
2024-12-28 10:07:28 -0500

    agregar cors para produccion

--- src/app.js
+++ src/app.js
@@ -13,8 +13,11 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));app.use(express.json());
 
 // Rutas
 app.use('/api/tasks', taskRoutes);
