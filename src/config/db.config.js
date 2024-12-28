632ccb8865ade3dd4328e55fe613d11a32ba67a9
jorcal123 <jcalambas5@gmail.com>
2024-12-28 10:20:40 -0500

    agregar cors para produccion

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     //await mongoose.connect(process.env.MONGODB_URI, {
-    const mongouri = process.env.MONGODB_URI || 'mongodb://localhost:27017/task-manager';
+    const mongouri = process.env.MONGODB_URI //|| 'mongodb://localhost:27017/task-manager';
     await mongoose.connect(mongouri);
       /*useNewUrlParser: true,
       useUnifiedTopology: true
