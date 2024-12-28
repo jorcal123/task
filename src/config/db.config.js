be1caf89b8d2c36ca3db3ce38a78c30d9e7fc962
jorcal123 <jcalambas5@gmail.com>
2024-12-28 17:44:19 -0500

    debuggear  problemas

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,16 +2,18 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-      //const mongouri = 'mongodb://localhost:27017/task-manager';
-      const mongouri = process.env.MONGODB_URI;
-      if (!mongouri) {
+    const mongouri = process.env.MONGODB_URI;
+    console.log('URI de conexión:', mongouri); // Para debuggear
+    
+    if (!mongouri) {
       throw new Error('La URL de la base de datos no está definida');
     }
+    
     await mongoose.connect(mongouri);
-
     console.log('Conexión a MongoDB establecida');
   } catch (error) {
     console.error('Error al conectar a MongoDB:', error.message);
+    console.error('Variables de entorno disponibles:', Object.keys(process.env)); // Para debuggear
     process.exit(1);
   }
 };
